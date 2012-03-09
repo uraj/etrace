62bcd385b79242abf6595a0e96b3bb8c34f69d03
Pei Wang <wangpei.cs@pku.edu.cn>
2012-03-09 14:54:56 +0800

    Initial commit

---
+++ get-module-ready.sh
@@ -0,0 +1,1 @@
+make ARCH=arm CROSS_COMPILE=/home/uraj/Workspace/android-source/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin/arm-eabi- -C ../htc-kernel-msm7x30/ SUBDIRS=$PWD modules
---
+++ upload-module.sh
@@ -0,0 +1,2 @@
+adb shell mount -o remount,rw /system
+adb push etrace.ko /system/lib/modules/etrace.ko
